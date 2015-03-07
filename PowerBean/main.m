7331e53ac0d8c9ca1ba29530eaa8ccf37c3c3a20
Adam Tindale <adam@adamtindale.com>
2015-03-07 14:00:44 -0500

    Initial Commit

---
+++ PowerBean/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PowerBean
+//
+//  Created by Adam Tindale on 2015-03-07.
+//  Copyright (c) 2015 Adam Tindale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "C4AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([C4AppDelegate class]));
+    }
+}
---
+++ PowerBeanTests/PowerBeanTests.m
@@ -0,0 +1,40 @@
+//
+//  PowerBeanTests.m
+//  PowerBeanTests
+//
+//  Created by Adam Tindale on 2015-03-07.
+//  Copyright (c) 2015 Adam Tindale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PowerBeanTests : XCTestCase
+
+@end
+
+@implementation PowerBeanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
